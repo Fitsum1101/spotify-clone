13b00eab9b67eab7e91138b64cd16193f7c0b3e6
Fitsum Kifle <fitsumt835@gmail.com>
2025-06-12 23:10:30 +0300

    adding provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,78 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import MusicPlayingProvider from "./context/MusicPlayingProvider.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
-[{
-    id: "1804419953",
-    type: "songs",
-    href: "/v1/catalog/dz/songs/1804419953?l=en-GB",
-    attributes: {
-      hasTimeSyncedLyrics: true,
-      albumName: "You'll Be Alright, Kid (Chapter 1)",
-      genreNames: ["Pop", "Music"],
-      trackNumber: 1,
-      releaseDate: "2024-09-26",
-      durationInMillis: 186964,
-      isVocalAttenuationAllowed: true,
-      isMasteredForItunes: true,
-      isrc: "USAT22500463",
-      artwork: {
-        width: 3000,
-        url: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/f1/91/6e/f1916ecb-0b21-57da-7d60-cd21ae0ff40b/075679614797.jpg/440x440bb.jpg",
-        height: 3000,
-        textColor3: "cdb192",
-        textColor2: "de8e78",
-        textColor4: "b47462",
-        textColor1: "fedab4",
-        bgColor: "0d0c0a",
-        hasP3: false,
-      },
-      audioLocale: "en-US",
-      composerName: "Adam Yaron, Alex Warren, Cal Shapiro & Mags Duval",
-      url: "https://music.apple.com/dz/album/ordinary/1804419951?i=1804419953",
-      playParams: {
-        id: "1804419953",
-        kind: "song",
-      },
-      discNumber: 1,
-      isAppleDigitalMaster: true,
-      hasLyrics: true,
-      audioTraits: ["lossless", "lossy-stereo"],
-      name: "Ordinary",
-      previews: [
-        {
-          url: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview211/v4/c1/a9/e1/c1a9e117-ad63-92f4-9e42-d1df1e34bdab/mzaf_5807637991739290764.plus.aac.p.m4a",
-        },
-      ],
-      artistName: "Alex Warren",
-    },
-    relationships: {
-      artists: {
-        href: "/v1/catalog/dz/songs/1804419953/artists?l=en-GB",
-        data: [
-          {
-            id: "1572671688",
-            type: "artists",
-            href: "/v1/catalog/dz/artists/1572671688?l=en-GB",
-          },
-        ],
-      },
-      "music-videos": {
-        href: "/v1/catalog/dz/songs/1804419953/music-videos?l=en-GB",
-        data: [
-          {
-            id: "1802070537",
-            type: "music-videos",
-            href: "/v1/catalog/dz/music-videos/1802070537?l=en-GB",
-          },
-        ],
-      },
-    },
-    meta: {
-      contentVersion: {
-        MZ_INDEXER: 1749013109082,
-        RTCI: 1749179489726,
-      },
-    },
-  },]
+createRoot(document.getElementById("root")).render(
+  <MusicPlayingProvider>
+    <App />
+  </MusicPlayingProvider>
+);
